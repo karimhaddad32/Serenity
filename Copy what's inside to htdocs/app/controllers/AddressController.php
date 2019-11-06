60747ca6c4e57033532cbca081b80a8ede24caad
Karim Haddad <karim.haddad@hotmail.com>
2019-11-06 08:13:45 -0500

    added the files and SQL file

---
+++ Copy what's inside to htdocs/app/controllers/AddressController.php
@@ -0,0 +1,80 @@
+<?php
+class AddressController extends Controller {
+
+	public function index() {
+	
+		$theAddress = $this->model('Address');
+		$theAddress->profile_id = $_SESSION['user_id'];
+		$theAddresses = $theAddress->getAll();
+
+		$this->view('Address/index', $theAddresses);
+	}
+
+	public function create() {
+
+		if(!isset($_POST['action'])) {
+
+			$this->view('Address/create');
+
+		} else {
+
+			$address = $this->model('Address');
+			$address->profile_id = $_SESSION['user_id'];
+        	$address->description = $_POST['description'];
+        	$address->street_address = $_POST['street_address'];
+        	$address->city = $_POST['city'];
+        	$address->province = $_POST['province'];
+        	$address->postal_code = $_POST['postal_code'];
+        	$address->country_id = $_POST['country_id'];
+
+			$address->insert();
+
+			header('location:/Address/index');
+		}
+	}
+
+	public function edit($address_id) {
+
+		$theAddress = $this->model('Address')->find($address_id);
+
+		if(!isset($_POST['action'])) {
+
+			$this->view('Address/edit', $theAddress);
+
+		} else {
+
+			$theAddress->profile_id = $_SESSION['user_id'];
+        	$theAddress->description = $_POST['description'];
+        	$theAddress->street_address = $_POST['street_address'];
+        	$theAddress->city = $_POST['city'];
+        	$theAddress->province = $_POST['province'];
+        	$theAddress->postal_code = $_POST['postal_code'];
+        	$theAddress->country_id = $_POST['country_id'];
+
+			$thePerson->update();
+
+			header('location:/Address/index');
+		}
+	}
+
+
+
+
+	public function delete($address_id) {
+
+		$theAddress = $this->model('Address')->find($address_id);
+
+		if(!isset($_POST['action'])){
+
+			$this->view('Default/delete', $address_id);
+
+		} else {
+
+			$theAddress->delete();
+
+			header('location:/Address/index');
+		}
+
+	}
+}
+?>
---
+++ Copy what's inside to htdocs/app/models/Address.php
@@ -0,0 +1,69 @@
+<?php
+class Address extends Model{
+    public $address_id;
+    public $profile_id;
+    public $description;
+    public $street_address;
+    public $city;
+    public $province;
+    public $postal_code;
+    public $country_id;
+
+    public function __construct()
+    {   
+        parent::__construct();
+    }
+
+	public function getAll() {
+        $stmt = self::$_connection->prepare("SELECT * FROM Address WHERE profile_id = :profile_id");
+        $stmt->execute(['profile_id' => $this->profile_id]);
+    	$stmt->setFetchMode(PDO::FETCH_CLASS, 'Address');
+		return $stmt->fetchAll();
+    }
+
+    public function find($address_id) {
+        $stmt = self::$_connection->prepare("SELECT * FROM Address WHERE address_id = :address_id");
+        $stmt->execute(['address_id' => $address_id]);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, 'Address');
+        return $stmt->fetch();
+    }
+
+    public function create() {
+	    $stmt = self::$_connection->prepare("INSERT INTO Address(description, street_address, city, province, postal_code, country_id)
+                                                  VALUES (:description, :street_address, :city, :province, :postal_code, :country_id)");
+        $stmt->execute(
+            ['description' => $this->description,
+             'street_address' => $this->street_address,
+             'city' => $this->city,
+             'province' => $this->province,
+             'postal_code' => $this->postal_code,
+             'country_id' => $this->country_id]);
+    }
+
+    public function edit() {
+        $stmt = self::$_connection->prepare("UPDATE Address
+
+                                                SET description    = :description,
+                                                    street_address = :street_address,
+                                                    city           = :city,
+                                                    province       = :province,
+                                                    postal_code    = :postal_code,
+                                                    country_id     = :country_id
+
+                                              WHERE profile_id     = :profile_id;");
+        $stmt->execute(
+            ['description' => $this->description,
+             'street_address' => $this->street_address,
+             'city' => $this->city,
+             'province' => $this->province,
+             'postal_code' => $this->postal_code,
+             'country_id' => $this->country_id,
+             'profile_id' => $this->profile_id]);
+    }
+
+    public function delete() {
+        $stmt = self::$_connection->prepare("DELETE FROM Address WHERE address_id = :address_id");
+        $stmt->execute(['address_id'=>$address_id]);
+    }
+}
+?>
