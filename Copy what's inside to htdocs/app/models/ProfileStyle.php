e2842650c9d1f96af93ead2998c353e92aa07dc2
Karim Haddad <karim.haddad@hotmail.com>
2019-11-20 13:38:43 -0500

    doing ma tables

--- Copy what's inside to htdocs/app/models/ProfileStyle.php
+++ Copy what's inside to htdocs/app/models/ProfileStyle.php
@@ -1,8 +1,11 @@
 <?php
 
-class _Person extends Model {
-	public $first_name;
-	public $last_name;
+class Profile_style extends Model {
+	public $profile_style_id;
+	public $profile_id;
+    public $background_image_id;
+    public $font_color_id;
+    public $font_style_id;
 
     public function __construct()
     {   
@@ -10,34 +13,34 @@ class _Person extends Model {
     }
 
 	public function getAll() {
-        $stmt = self::$_connection->prepare("SELECT * FROM Person");
+        $stmt = self::$_connection->prepare("SELECT * FROM Profile_style");
         $stmt->execute();
-    	$stmt->setFetchMode(PDO::FETCH_CLASS, '_Person');
+    	$stmt->setFetchMode(PDO::FETCH_CLASS, 'Profile_style');
 		return $stmt->fetchAll();
     }
 
-    public function find($person_id) {
-        $stmt = self::$_connection->prepare("SELECT * FROM Person WHERE person_id = :person_id");
-        $stmt->execute(['person_id'=>$person_id]);
-        $stmt->setFetchMode(PDO::FETCH_CLASS, '_Person');
+    public function find() {
+        $stmt = self::$_connection->prepare("SELECT * FROM Profile_style WHERE profile_style_id = :profile_style_id");
+        $stmt->execute(['profile_style_id'=>$profile_style_id]);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, 'Profile_style');
         return $stmt->fetch();
     }
 
     public function insert() {
-	    $stmt = self::$_connection->prepare("INSERT INTO Person(first_name, last_name) VALUES(:first_name,:last_name)");
-        $stmt->execute(['first_name'=>$this->first_name,
-         'last_name'=>$this->last_name]);
+	    $stmt = self::$_connection->prepare("INSERT INTO Profile_style(profile_id, background_image_id,font_color_id , font_style_id) VALUES(:profile_id,:background_image_id,:font_color_id,:font_style_id)");
+        $stmt->execute(['profile_id'=>$this->profile_id,
+         'background_image_id'=>$this->background_image_id,'font_color_id'=>$this->font_color_id,'font_style_id'=>$this->font_style_id]);
     }
 
     public function delete() {
-        $stmt = self::$_connection->prepare("DELETE FROM Person WHERE person_id = :person_id");
-        $stmt->execute(['person_id'=>$this->person_id]);
+        $stmt = self::$_connection->prepare("DELETE FROM Profile_style WHERE profile_style_id = :profile_style_id AND profile_id = : profile_id");
+        $stmt->execute(['profile_style_id'=>$this->profile_style_id, 'profile_id'=>$this->profile_id]);
     }
 
     public function update() {
-        $stmt = self::$_connection->prepare("UPDATE Person SET first_name = :first_name, last_name = :last_name WHERE person_id = :person_id");
-        $stmt->execute(['first_name'=>$this->first_name,
-         'last_name'=>$this->last_name, 'person_id'=>$this->person_id]);
+        $stmt = self::$_connection->prepare("UPDATE Profile_style SET background_image_id = :background_image_id, font_color_id = :font_color_id,font_style_id = :font_style_id  WHERE profile_id = :profile_id AND profile_style_id = :profile_style_id ");
+        $stmt->execute(['background_image_id'=>$this->background_image_id,
+         'font_color_id'=>$this->font_color_id, 'font_style_id'=>$this->font_style_id,'profile_id'=>$this->profile_id,'profile_style_id'=>$this->profile_style_id]);
     }
 
 }
--- Copy what's inside to htdocs/app/models/ReactionType.php
+++ Copy what's inside to htdocs/app/models/ReactionType.php
@@ -1,8 +1,10 @@
 <?php
 
-class _Person extends Model {
-	public $first_name;
-	public $last_name;
+class Reaction_type extends Model {
+	public $reation_type_id;
+	public $reaction_description;
+    public $reaction_shortcut;
+    public $reaction_path;
 
     public function __construct()
     {   
@@ -10,36 +12,11 @@ class _Person extends Model {
     }
 
 	public function getAll() {
-        $stmt = self::$_connection->prepare("SELECT * FROM Person");
+        $stmt = self::$_connection->prepare("SELECT * FROM Reaction_type");
         $stmt->execute();
-    	$stmt->setFetchMode(PDO::FETCH_CLASS, '_Person');
+    	$stmt->setFetchMode(PDO::FETCH_CLASS, 'Reaction_type');
 		return $stmt->fetchAll();
     }
-
-    public function find($person_id) {
-        $stmt = self::$_connection->prepare("SELECT * FROM Person WHERE person_id = :person_id");
-        $stmt->execute(['person_id'=>$person_id]);
-        $stmt->setFetchMode(PDO::FETCH_CLASS, '_Person');
-        return $stmt->fetch();
-    }
-
-    public function insert() {
-	    $stmt = self::$_connection->prepare("INSERT INTO Person(first_name, last_name) VALUES(:first_name,:last_name)");
-        $stmt->execute(['first_name'=>$this->first_name,
-         'last_name'=>$this->last_name]);
-    }
-
-    public function delete() {
-        $stmt = self::$_connection->prepare("DELETE FROM Person WHERE person_id = :person_id");
-        $stmt->execute(['person_id'=>$this->person_id]);
-    }
-
-    public function update() {
-        $stmt = self::$_connection->prepare("UPDATE Person SET first_name = :first_name, last_name = :last_name WHERE person_id = :person_id");
-        $stmt->execute(['first_name'=>$this->first_name,
-         'last_name'=>$this->last_name, 'person_id'=>$this->person_id]);
-    }
-
 }
 
 ?>
